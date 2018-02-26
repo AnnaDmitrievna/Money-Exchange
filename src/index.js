baaf05d5b63e674d3c79aaba58902f99d1c1e758
Anna <annadmitrievna1823@gmail.com>
2018-02-26 16:40:05 +0300

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let resObj = {};
+    let currencies = [50,25,10,5,1];
+    let names = ['H','Q','D','N','P'];
+
+    if (currency > 10000) {
+        return {error: "You are rich, my friend! We don't have so much coins for exchange"}
+    }
+    currencies.reduce((current, item, index) => {
+        if (current >= item && current > 0) {
+            let summ = Math.floor(current/item);
+            resObj[names[index]] = summ;
+            return current - summ*item;
+        }
+        else {
+            return current
+        }
+    }, currency)
+
+
+    return resObj;
 }
